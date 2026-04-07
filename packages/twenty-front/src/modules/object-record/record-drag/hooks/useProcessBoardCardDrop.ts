51ce48142d2d22ab3ceaff8efcaa7b3dff0c8e25
Dhruv Raina <dhruv@fusegtm.com>
2026-04-07 19:46:36 -0400

    Fix board drag and drop issues (#19005)

--- packages/twenty-front/src/modules/object-record/record-drag/hooks/useProcessBoardCardDrop.ts
+++ packages/twenty-front/src/modules/object-record/record-drag/hooks/useProcessBoardCardDrop.ts
@@ -5,9 +5,12 @@ import { useCallback, useContext } from 'react';
 import { processGroupDrop } from '@/object-record/record-drag/utils/processGroupDrop';
 
 import { RecordBoardContext } from '@/object-record/record-board/contexts/RecordBoardContext';
+import { isRecordBoardDropProcessingComponentState } from '@/object-record/record-board/states/isRecordBoardDropProcessingComponentState';
 import { useUpdateDroppedRecordOnBoard } from '@/object-record/record-drag/hooks/useUpdateDroppedRecordOnBoard';
 import { recordIndexRecordIdsByGroupComponentFamilyState } from '@/object-record/record-index/states/recordIndexRecordIdsByGroupComponentFamilyState';
 import { useAtomComponentFamilyStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentFamilyStateCallbackState';
+import { useAtomComponentStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateCallbackState';
+import { useDebouncedCallback } from 'use-debounce';
 
 export const useProcessBoardCardDrop = () => {
   const store = useStore();
@@ -19,6 +22,21 @@ export const useProcessBoardCardDrop = () => {
     );
 
   const { updateDroppedRecordOnBoard } = useUpdateDroppedRecordOnBoard();
+
+  const isRecordBoardDropProcessingCallbackState =
+    useAtomComponentStateCallbackState(
+      isRecordBoardDropProcessingComponentState,
+    );
+
+  // TODO: this is necessary to avoid race conditions when dragging right after a previous drag (~200ms to 500ms)
+  // A way to fix this would be to have a proper optimistic logic on drop that doesn't just resets the whole board with trigger initial query but updates everything without waiting for the request return
+  // Which is the problem here because it kind of destroys the existing columns that have more records than page size, and dnd library has issues computing drag when the underlying data change.
+  const debouncedUpdateDropProcessing = useDebouncedCallback(
+    (isPending: boolean) => {
+      store.set(isRecordBoardDropProcessingCallbackState, isPending);
+    },
+    500,
+  );
 
   const processBoardCardDrop = useCallback(
     (boardCardDropResult: DropResult, selectedRecordIds: string[]) => {
@@ -37,12 +55,15 @@ export const useProcessBoardCardDrop = () => {
           );
         },
       });
+
+      debouncedUpdateDropProcessing(false);
     },
     [
       store,
       selectFieldMetadataItem,
       recordIndexRecordIdsByGroupCallbackFamilyState,
       updateDroppedRecordOnBoard,
+      debouncedUpdateDropProcessing,
     ],
   );
 
