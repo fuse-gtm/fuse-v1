8e498664bf669c602222f4193e610968bfbac472
Dhruv Raina <dhruv@fusegtm.com>
2026-04-07 22:40:54 -0400

    feat: optimize hot database queries with multi-layer caching (#19068)

--- packages/twenty-server/src/engine/core-entity-cache/core-entity-cache.module.ts
+++ packages/twenty-server/src/engine/core-entity-cache/core-entity-cache.module.ts
@@ -1,5 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DiscoveryModule } from '@nestjs/core';
 
-// TODO: upstream cherry-pick stub - module not yet in fork
-@Module({ providers: [], exports: [] })
+import { CacheStorageModule } from 'src/engine/core-modules/cache-storage/cache-storage.module';
+import { CoreEntityCacheService } from 'src/engine/core-entity-cache/services/core-entity-cache.service';
+
+@Module({
+  imports: [CacheStorageModule, DiscoveryModule],
+  providers: [CoreEntityCacheService],
+  exports: [CoreEntityCacheService],
+})
 export class CoreEntityCacheModule {}
