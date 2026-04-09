6c412eb119bc582c61119634cd6da2bb7c0100e1
Dhruv Raina <dhruv@fusegtm.com>
2026-04-09 13:31:34 -0400

    style(ci): format shared files with repo prettier

--- packages/twenty-shared/src/application/fieldManifestType.ts
+++ packages/twenty-shared/src/application/fieldManifestType.ts
@@ -26,8 +26,8 @@ export type RegularFieldManifest<
 };
 
 export type RelationFieldManifest<
-  T extends RelationAndMorphRelationFieldMetadataType =
-    RelationAndMorphRelationFieldMetadataType,
+  T extends
+    RelationAndMorphRelationFieldMetadataType = RelationAndMorphRelationFieldMetadataType,
 > = Omit<RegularFieldManifest<T>, 'universalSettings' | 'type'> & {
   type: T;
   relationTargetFieldMetadataUniversalIdentifier: string;
