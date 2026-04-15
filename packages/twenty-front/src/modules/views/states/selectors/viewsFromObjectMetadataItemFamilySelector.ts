b4813e1a59f5ba151d4bfc476ad9518ebdf11264
Etienne <45695613+etiennejouan@users.noreply.github.com>
2026-04-15 15:30:37 +0200

    Object view widget - Introduce new TABLE_WIDGET view type (#19545)
    closes
    https://discord.com/channels/1130383047699738754/1491549365263667230/1491804729397743666
    
    (cherry picked from commit 94b8e34362ce7a8cb1d5ede456f0a6399af383e4)

--- packages/twenty-front/src/modules/views/states/selectors/viewsFromObjectMetadataItemFamilySelector.ts
+++ packages/twenty-front/src/modules/views/states/selectors/viewsFromObjectMetadataItemFamilySelector.ts
@@ -1,22 +1,21 @@
 import { createAtomFamilySelector } from '@/ui/utilities/state/jotai/utils/createAtomFamilySelector';
-import { coreViewsSelector } from '@/views/states/selectors/coreViewsSelector';
+import { viewsSelector } from '@/views/states/selectors/viewsSelector';
 import { type View } from '@/views/types/View';
 import { ViewType } from '@/views/types/ViewType';
-import { convertCoreViewToView } from '@/views/utils/convertCoreViewToView';
 
-export const coreViewsFromObjectMetadataItemFamilySelector =
+export const viewsFromObjectMetadataItemFamilySelector =
   createAtomFamilySelector<View[], { objectMetadataItemId: string }>({
-    key: 'coreViewsFromObjectMetadataItemFamilySelector',
+    key: 'viewsFromObjectMetadataItemFamilySelector',
     get:
       ({ objectMetadataItemId }) =>
       ({ get }) => {
-        const coreViews = get(coreViewsSelector);
-        const views = coreViews.map(convertCoreViewToView);
+        const views = get(viewsSelector);
         return views
           .filter(
             (view) =>
               view.objectMetadataId === objectMetadataItemId &&
-              view.type !== ViewType.FieldsWidget,
+              view.type !== ViewType.FIELDS_WIDGET &&
+              view.type !== ViewType.TABLE_WIDGET,
           )
           .sort((a, b) => a.position - b.position);
       },
